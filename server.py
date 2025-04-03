928fbc4adce4d6e433d7ecc69d4209c2debf74a4
Matúš Zeleňák <matuszelenak@protonmail.com>
2025-04-03 14:39:20 +0200

    Enable relative bboxes, remove unnecessary params

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import io
 import logging
-from typing import List
-import torch
+from typing import List, Literal
 
+import torch
 from PIL import Image
 from fastapi import FastAPI, Form
 from fastapi import File, UploadFile
@@ -17,27 +17,26 @@ gdino = GDINO()
 gdino.build_model()
 
 
-class PanelSegmentationParameters(BaseModel):
+class SegmentationParameters(BaseModel):
     box_threshold: float
     text_threshold: float
     text_prompt: str
-    min_width: float
-    max_width: float
-    min_height: float
-    max_height: float
-    avg_color_darkness_limit: float
+    bbox_type: Literal['absolute', 'relative'] = 'absolute'
 
 
 class BoundingBoxModel(BaseModel):
+    id: int
     center_x: float
     center_y: float
     width: float
     height: float
+    value_type: Literal['absolute', 'relative']
 
 
 @app.post("/segment")
-async def segment_image(parameters: str = Form(...), photos: List[UploadFile] = File(...)) -> List[List[BoundingBoxModel]]:
-    model_params = PanelSegmentationParameters.model_validate_json(parameters)
+async def segment_image(parameters: str = Form(...), photos: List[UploadFile] = File(...)) -> List[
+    List[BoundingBoxModel]]:
+    model_params = SegmentationParameters.model_validate_json(parameters)
 
     images = []
     for photo in photos:
@@ -52,22 +51,35 @@ async def segment_image(parameters: str = Form(...), photos: List[UploadFile] = 
         [model_params.text_prompt] * len(images),
         model_params.box_threshold,
         model_params.text_threshold,
-    )
+        )
 
     torch.cuda.empty_cache()
 
     images_bboxes = []
-    for idx, image_result in enumerate(gdino_results):
+    for idx, (image, image_result) in enumerate(zip(images, gdino_results)):
         bboxes = []
         if image_result["labels"]:
             image_result["boxes"] = image_result["boxes"].cpu().numpy()
 
-            for box in image_result["boxes"]:
+            for _id, box in enumerate(image_result["boxes"]):
+                center_x = box[0] + (box[2] - box[0]) / 2
+                center_y = box[1] + (box[3] - box[1]) / 2
+                width = box[2] - box[0]
+                height = box[3] - box[1]
+
+                if model_params.bbox_type == 'relative':
+                    center_x /= image.width
+                    center_y /= image.height
+                    width /= image.width
+                    height /= image.height
+
                 box_instance = BoundingBoxModel(
-                    center_x=box[0] + (box[2] - box[0]) / 2,
-                    center_y=box[1] + (box[3] - box[1]) / 2,
-                    width=box[2] - box[0],
-                    height=box[3] - box[1],
+                    id=_id,
+                    center_x=center_x,
+                    center_y=center_y,
+                    width=width,
+                    height=height,
+                    value_type=model_params.bbox_type
                 )
                 bboxes.append(box_instance)
 
